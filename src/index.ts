4fd7c14cf788acacaabdaf9ebeb4b414dcd3f76f
Files Manager <lqvu99+webapi@gmail.com>
2020-09-11 00:48:46 +0700

    Use consts:dev instead of process.env.NODE_ENV

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ app.use(
 
 app.use(morgan('short'));
 
-if (process.env.NODE_ENV === 'production') {
+if (!dev) {
     app.use((req, res, next) => {
         if (req.header('x-forwarded-proto') !== 'https')
             res.redirect(`https://${req.header('host')}${req.url}`);
