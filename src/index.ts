e6cbaca5016c9feb4f4730f889430b420c15b092
Files Manager <lqvu99+webapi@gmail.com>
2020-09-13 22:44:29 +0700

    Rename uploadFile -> uploadFiles

--- src/index.ts
+++ src/index.ts
@@ -5,46 +5,45 @@ import path from 'path';
 import dev from 'consts:dev';
 
 import { auth } from './middleware/auth';
-import { uploadFileHandler } from './routes/uploadFileHandler';
-import cfg from '../config.json';
 import { getFilesIndex } from './filesIndex';
-import { uploadFolderPath } from './config';
+import { uploadFolderPath, config } from './config';
 import { getFilesListHandler } from './routes/getFilesList';
+import { uploadFilesHandler } from './routes/uploadFilesHandler';
 
 global.PORT = process.env.PORT || 4999;
 const app = express();
 
 app.use(
-    cors({
-        origin: ([/https?:\/\/localhost:?\d+/i] as (string | RegExp)[]).concat(
-            cfg.origin
-        ),
-    })
+	cors({
+		origin: ([/https?:\/\/localhost:?\d+/i] as (string | RegExp)[]).concat(
+			config.origin
+		),
+	})
 );
 
 app.use(morgan('short'));
 
 if (!dev) {
-    app.use((req, res, next) => {
-        if (req.header('x-forwarded-proto') !== 'https')
-            res.redirect(`https://${req.header('host')}${req.url}`);
-        else next();
-    });
+	app.use((req, res, next) => {
+		if (req.header('x-forwarded-proto') !== 'https')
+			res.redirect(`https://${req.header('host')}${req.url}`);
+		else next();
+	});
 }
 
 app.use(auth());
-app.use('/uploadFile', uploadFileHandler);
+app.use('/uploadFiles', uploadFilesHandler);
 app.use('/filesList', getFilesListHandler);
 app.get('/', (req, res) => {
-    res.sendFile(path.join(process.cwd(), './src/page.html'));
+	res.sendFile(path.join(process.cwd(), './src/page.html'));
 });
 
 if (dev) {
-    app.use('/publish', express.static(uploadFolderPath));
+	app.use('/publish', express.static(uploadFolderPath));
 }
 
 getFilesIndex().then(() => {
-    app.listen(global.PORT, () => {
-        console.log('Server started! Listening at port: ' + global.PORT);
-    });
+	app.listen(global.PORT, () => {
+		console.log('Server started! Listening at port: ' + global.PORT);
+	});
 });
