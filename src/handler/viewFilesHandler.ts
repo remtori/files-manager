35147f0176c08767ceef85d13b0f476c1f8aceb5
Files Manager <lqvu99+webapi@gmail.com>
2020-10-03 20:24:05 +0700

    Added query in the URL for persistent config

--- src/handler/viewFilesHandler.ts
+++ src/handler/viewFilesHandler.ts
@@ -1,3 +1,4 @@
+import { encode as queryEncode, decode as queryDecode } from 'querystring';
 import { Request, Response, Router } from 'express';
 import { getFilesIndex } from '../filesIndex';
 import { getFileNode } from '../utils';
@@ -13,15 +14,30 @@ viewFilesHandler.get('/*', async (req: Request, res: Response) => {
 
     const queryIndex = req.originalUrl.indexOf('?');
     const queryString = queryIndex >= 0 ? req.originalUrl.slice(queryIndex) : '';
+    const queryObject = queryDecode(queryString.slice(1));
 
     const paths = req.path.split('/');
     const urlPaths = paths.slice(0);
     paths[0] = 'root';
 
     res.render('template', {
+        config: Object.assign({
+            display: 'list',
+            sort: 'name',
+            order: 'ascending',
+        }, queryObject),
+        request: req,
         paths,
         fileNode,
         queryString,
-        urlPaths: urlPaths.map((_, i) => `${urlPaths.slice(0, i + 1).join('/')}${queryString}`),
+        queryObject,
+        urlPaths: urlPaths.map((_, i) => `${urlPaths.slice(0, i + 1).join('/')}`),
+        setQuery: (key: string, value: string) => {
+            return '?' + queryEncode(
+                Object.assign({}, queryObject, {
+                    [key]: value,
+                })
+            );
+        },
     });
 });
