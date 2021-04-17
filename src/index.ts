d17f9eae62be81f7877776f024a34cc5271c410e
remtori <lqvu99@gmail.com>
2021-04-18 00:30:32 +0700

    Remove auth

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import cors from 'cors';
 import path from 'path';
 import dev from 'consts:dev';
 
-import { auth } from './middleware/auth';
 import { getFilesIndex } from './filesIndex';
 import { config } from './config';
 import { uploadFilesHandler } from './handler/uploadFilesHandler';
@@ -25,7 +24,6 @@ app.use(morgan('short'));
 if (!dev) app.use(onlyHttps);
 app.set('view engine', 'ejs');
 
-app.use(auth());
 app.use('/explorer', viewFilesHandler);
 app.use('/files', uploadFilesHandler);
 app.use('/files', rawFilesHandler);
